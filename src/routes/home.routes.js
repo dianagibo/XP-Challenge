fc2f47e68db29a54e24849d18004ac9c1d048c5d
Diana Giraldo <66281949+dianagibo@users.noreply.github.com>
2026-07-23 16:32:17 -0500

    fix: harden mobile login redirect

--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -5,10 +5,9 @@ const { allowRoles } = require('../middleware/authorization');
 
 const router = express.Router();
 
-router.use(requireAuthentication);
-router.get('/', homeController.showDashboard);
-router.get('/avatars', allowRoles('admin_player', 'player', 'player_validator'), homeController.showAvatars);
-router.get('/rewards', allowRoles('admin_player', 'player', 'player_validator'), homeController.showRewards);
-router.get('/progress', allowRoles('admin_player', 'player', 'player_validator'), homeController.showProgress);
+router.get('/', requireAuthentication, homeController.showDashboard);
+router.get('/avatars', requireAuthentication, allowRoles('admin_player', 'player', 'player_validator'), homeController.showAvatars);
+router.get('/rewards', requireAuthentication, allowRoles('admin_player', 'player', 'player_validator'), homeController.showRewards);
+router.get('/progress', requireAuthentication, allowRoles('admin_player', 'player', 'player_validator'), homeController.showProgress);
 
 module.exports = router;
